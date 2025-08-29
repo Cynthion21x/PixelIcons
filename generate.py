ccff882d571c1f9a8e55a694acf6c16891968dd4
Cynthion <cynthion21x@gmail.com>
2025-08-29 23:31:42 +0100

    generator scripts for small icons

--- generate.py
+++ generate.py
@@ -1,12 +1,13 @@
 import os
 from PIL import Image
 
-sizes = [16, 24, 32, 48, 64, 96, 128, 256, 512]
+sizes = [32, 48, 64, 96, 128, 256, 512]
+sizes_l = [16, 24]
 
 input = 'apps/icons'
+input_s = 'apps/icons-small'
 output = 'apps'
 
-# make folders
 for size in sizes:
     os.makedirs(os.path.join(output, str(size)), exist_ok=True)
 
@@ -17,12 +18,36 @@ for filename in os.listdir(input):
 
     inputF = os.path.join(input, filename)
     with Image.open(inputF) as img:
+
+        print("resizing " + inputF)
+
         for size in sizes:
 
-            # scale using nearest neighbour so they are not blurry
             resized = img.resize((size, size), resample=Image.NEAREST)
             outputF = os.path.join(output, str(size), filename)
             resized.save(outputF)
 
-print("Generated icons :D")
+print("Generated large icons")
 
+for size in sizes_l:
+    os.makedirs(os.path.join(output, str(size)), exist_ok=True)
+
+for filename in os.listdir(input_s):
+
+    if not filename.lower().endswith('.png'):
+        continue
+
+    inputF = os.path.join(input_s, filename)
+    with Image.open(inputF) as img:
+
+        print("resizing " + inputF)
+
+        for size in sizes_l:
+            
+            resized = img.resize((size, size), resample=Image.NEAREST)
+            outputF = os.path.join(output, str(size), filename)
+            resized.save(outputF)
+
+print("Generated small icons")
+
+print("finished :D")
